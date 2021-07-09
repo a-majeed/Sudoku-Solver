851057a1a6c3bcde992863f1d65c39d043895bd0
Azaan Majeed <azaan@ualberta.ca>
2021-07-08 19:24:37 -0600

    added function to check if inserted number is valid

--- working.py
+++ working.py
@@ -30,10 +30,45 @@ def display_board(bo):
 
 
 def find_available(bo):
+    
     for i in range(len(bo)):
             for j in range(len(bo[0])):
                 if bo[i][j] == 0:
                     return (i,j) 
+    
+    return None
+
+
+def valid(bo, num, pos):
+
+    #checking rows
+    for i in range(len(bo[0])):
+        if bo[pos[0]][i] == num and pos[1] != i:
+            return False
+    
+    #checking columns
+    for i in range(len(bo)):
+        if bo[i][pos[1]] == num and pos[0] != i:
+            return False
+    
+    #check 3x3 box
+    box_x = pos[1] // 3
+    box_y = pos[0] // 3
+
+    for i in range(3*box_y, 3*box_y +3):
+        for j in range(3*box_x, 3*box_x +3):
+            if bo[i][j] == num and (i,j) != pos:
+                return False
+
+    return True
+
+
+    
+
+
+        
+
+
 
 
 
